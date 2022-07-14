48b885cc2c583b70d344dcd4908654b357332fc6
Lukas Berger <lukas@grovia.de>
2022-07-14 14:17:59 +0200

    added sort merge join

--- hash_join.py
+++ hash_join.py
@@ -1,17 +1,10 @@
-from collections import defaultdict
 import hashlib
-from time import sleep
-from attr import has
-import numpy as np
-
-from typing import Dict
-
-from pandas import array
+from typing import Dict, Tuple, List
 
 def get_file_size(path: str) -> int:
     return sum(1 for line in open(path))
 
-def get_tables(path: str) -> Dict[str, np.array]:
+def get_tables(path: str) -> Dict[str, List[Tuple[int, int]]]:
     file_size = get_file_size(path)
     string_dict = {}
     tables = {}
@@ -30,34 +23,66 @@ def get_tables(path: str) -> Dict[str, np.array]:
                 string_dict[property_hash] = property
                 string_dict[object_hash] = object
                 if property not in tables:            
-                    tables[property] = np.ndarray((1, 2), buffer=np.array([[subject_hash, object_hash]]), dtype=int)
+                    tables[property] = [(subject_hash, object_hash)]
                 else:
-                    tables[property] = np.append(tables[property], np.array([[subject_hash, object_hash]]), axis=0)
+                    tables[property].append((subject_hash, object_hash))
     return tables, string_dict
 
 def get_hash(value: str) -> int:
     return int(hashlib.md5(value.encode("utf-8")).hexdigest(), 16)
 
-def hash_join(table_1: np.ndarray, column_1: int, table_2 : np.ndarray, column_2: int, word_dict):
+def hash_join(table_1: List[Tuple[int, int]], column_1: int, table_2 : List[Tuple[int, int]], column_2: int):
     # hash phase
     hash_map = dict()
     for x in table_1:
         key = x[column_1]
         if key not in hash_map:
-            hash_map[key] = np.ndarray((1, 2), buffer=np.array([x]), dtype=int)
+            hash_map[key] = [x]
         else:
-            hash_map[key] = np.append(hash_map[key], np.array([x]), axis=0)
+            hash_map[key].append(x)
 
     # join phase
-    hash_map = {int(k): v for k, v in hash_map.items()}
-    result = np.ndarray((1, 4), dtype=int)
+    result = []
     for row1 in table_2:
         key = row1[column_2] 
         if key in hash_map:
             for row2 in hash_map[key]:
-                row = np.append(row1, row2)
-                result = np.append(result, np.array([row]), axis=0)
+                result.append((row1, row2))
     return result
+
+def sort_merge_join(table_1 : List[Tuple[int, int]], index_1: int,
+                    table_2: List[Tuple[int, int]], index_2: int) -> List[Tuple[int, int]]:
+    # sort values
+    table_1.sort(key=lambda x: x[index_1])
+    table_2.sort(key=lambda x: x[index_2])
+
+    # merge value
+    i = 0
+    j = 0
+    i_max = len(table_1) - 1
+    j_max = len(table_2) - 1
+    output_table = []
+    while i <= i_max and j <= j_max: 
+        if table_1[i][index_1] > table_2[j][index_2]:
+            j += 1
+        elif table_1[i][index_2] < table_2[j][index_2]:
+            i += 1
+        else:
+            output_table.append([table_1[i], table_2[j]])
+            j_prime = j + 1
+            while j_prime <= j_max and table_1[i][index_1] == table_2[j_prime][index_2]:
+                output_table.append([table_1[i], table_2[j_prime]])
+                j_prime += 1
+            i_prime = i + 1
+            while i_prime <= i_max and table_1[i_prime][index_1] == table_2[j_prime][index_2]:
+                output_table.append([table_1[i_prime], table_2[j]])
+                i_prime += 1
+            i = i_prime
+            j = j_prime
+    return output_table
+            
+
+
 
 if __name__ == "__main__":
     tables, string_dict = get_tables("100k.txt")
@@ -67,4 +92,4 @@ if __name__ == "__main__":
     print(len(friend_of_table))
     print(len(follows_table))
     
-    print(hash_join(follows_table, 1, friend_of_table, 1, string_dict))
+    print(sort_merge_join(follows_table, 0, friend_of_table, 1))
